0451ff7c5da85c8272ed3cc95e574dc711638377
Brent Sullivan <brntsllvn@gmail.com>
2016-02-15 11:00:26 -0800

    clean up specs

---
+++ spec/factories/comics.rb
@@ -0,0 +1,18 @@
+FactoryGirl.define do
+  factory :comic do
+    sequence(:title) { |n| 'Awesome Comic #{n}' }
+    release_date Date.new(1999, 12, 31)
+    # carrierwave requires actual file (not just a string)
+    # Detail: http://stackoverflow.com/questions/14227086/what-parameters-do-i-need-to-pass-in-the-controller-to-a-carrierwave-mounted-mod
+    pic { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'uploaders', 'awesome.png')) }
+  
+    factory :invalid_comic do
+      pic nil
+    end
+
+    factory :edited_comic do
+      title 'Amazing Comic'
+      release_date Date.new(1999, 01, 01)
+    end
+  end
+end
