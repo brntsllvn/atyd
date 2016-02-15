49ffe1975f37dc832293ccd0636f41ab998a1379
Brent Sullivan <brntsllvn@gmail.com>
2016-02-14 20:28:54 -0800

    comic validations

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,3 +1,7 @@
 class Comic < ActiveRecord::Base
+  validates :title,        presence: true
+  validates :release_date, presence: true
+  validates :pic,          presence: true
+
   mount_uploader :pic, ComicUploader
 end
