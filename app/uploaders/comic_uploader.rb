a0e64070cf2e109b2611b82ad7103f049e1c948b
Brent Sullivan <brntsllvn@gmail.com>
2016-02-14 17:45:56 -0800

    carrierwave specs

--- app/uploaders/comic_uploader.rb
+++ app/uploaders/comic_uploader.rb
@@ -14,7 +14,8 @@ class ComicUploader < CarrierWave::Uploader::Base
   # end
 
   # Choose what kind of storage to use for this uploader:
-  storage :aws
+  # Remember, if you have already set storage :something in your uploader, the storage setting from this initializer will be ignored.
+  # storage :aws
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
---
+++ spec/uploaders/comic_uploader_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+require 'carrierwave/test/matchers'
+
+describe ComicUploader do
+  include CarrierWave::Test::Matchers
+
+  # Enable images processing before executing the examples.
+  # Create a new uploader.
+  before(:each) do
+    ComicUploader.enable_processing = true
+    @comic = Comic.new
+    @uploader = ComicUploader.new(@comic, :pic)
+    @uploader.store!(File.open(Rails.root.join('spec', 'uploaders', 'awesome.png')))
+  end
+
+  # Disable images processing after executing the examples
+  after(:each) do
+    ComicUploader.enable_processing = false
+  end
+
+  it "resizes the image" do
+    expect(@uploader).to have_dimensions(1200, 1200)
+  end
+end   
