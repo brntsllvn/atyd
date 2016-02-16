f67dba7c59f72ca7dfa3e4a34c3e3f34511a8bb8
Brent Sullivan <brntsllvn@gmail.com>
2016-02-15 17:08:02 -0800

    mailer specs and garbage collection

--- spec/mailers/comic_mailer_spec.rb
+++ spec/mailers/comic_mailer_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe ComicMailer, type: :mailer do
 
   it "#new_comic" do
     ComicMailer.new_comic.deliver_now
-    expect(ActionMailer::Base.deliveries.last.to).to eq ['brntsllvn@gmail.com']
-    expect(ActionMailer::Base.deliveries.last.from).to eq ['acetheyellowdog@gmail.com']
-    expect(ActionMailer::Base.deliveries.last.subject).to eq 'Ace The Yellow Dog: New Comic'
+    expect(last_email.to).to eq ['brntsllvn@gmail.com']
+    expect(last_email.from).to eq ['acetheyellowdog@gmail.com']
+    expect(last_email.subject).to eq 'Ace The Yellow Dog: New Comic'
   end
 end
