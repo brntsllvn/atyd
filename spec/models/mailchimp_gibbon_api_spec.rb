b8649cd9111693ff48292edd0cd30f4ed1ed59ac
Brent Sullivan <brntsllvn@gmail.com>
2016-02-17 08:41:35 -0800

    comment gibbon specs

--- spec/models/mailchimp_gibbon_api_spec.rb
+++ spec/models/mailchimp_gibbon_api_spec.rb
@@ -1,26 +1,26 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe Gibbon::APIRequest, type: :model do
+# RSpec.describe Gibbon::APIRequest, type: :model do
 
-  describe "gibbon" do
-    before :each do
-      @gibbon = Gibbon::Request.new
-    end
+#   describe "gibbon" do
+#     before :each do
+#       @gibbon = Gibbon::Request.new
+#     end
 
-    it "returns instance of Gibbon::Request" do
-      expect(@gibbon).to be_an_instance_of(Gibbon::Request)
-    end
+#     it "returns instance of Gibbon::Request" do
+#       expect(@gibbon).to be_an_instance_of(Gibbon::Request)
+#     end
 
-    it "returns array of lists" do
-      lists = @gibbon.lists.retrieve
-      expect(lists).to be_a(Hash)
-      expect(lists.count).to eq 3 # my lists + mailchimp admin list
-    end
+#     it "returns array of lists" do
+#       lists = @gibbon.lists.retrieve
+#       expect(lists).to be_a(Hash)
+#       expect(lists.count).to eq 3 # my lists + mailchimp admin list
+#     end
 
-    it "returns test list" do
-      test_list = @gibbon.lists(ENV["MAILCHIMP_LIST_ID_TEST"]).retrieve 
-      expect(test_list['name']).to eq "rails list"
-    end
-  end
+#     it "returns test list" do
+#       test_list = @gibbon.lists(ENV["MAILCHIMP_LIST_ID_TEST"]).retrieve 
+#       expect(test_list['name']).to eq "rails list"
+#     end
+#   end
 
-end
+# end
