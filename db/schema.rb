293e0594367d19eef9fba7b156b6888dfc4ec0e8
Brent Sullivan <brntsllvn@gmail.com>
2016-02-18 22:44:35 -0800

    session specs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219035152) do
+ActiveRecord::Schema.define(version: 20160219050054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160219035152) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.string   "image"
+    t.string   "email"
   end
 
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,19 +1,59 @@
-# require 'rails_helper'
+require 'rails_helper'
+require 'pry'
 
-# RSpec.describe SessionsController, type: :controller do
+RSpec.describe SessionsController, type: :controller do
 
-#   describe "GET #create" do
-#     it "returns http success" do
-#       get :create
-#       expect(response).to have_http_status(:redirect)
-#     end
-#   end
+  before :each do
+    # messy mock
+    request.env["omniauth.auth"] = 
+      OmniAuth::AuthHash.new(
+      { 'provider' => 'google',
+        'uid' => '12345',
+          'info' => {
+            'name' => 'natasha',
+            'email' => 'hi@natashatherobot.com',
+            'image' => '123456789.png'
+          },
+          'credentials' => {
+            'token' => '123USA',
+            'expires_at' => 12345
+          }
+      })
+  end
 
-#   describe "GET #destroy" do
-#     it "returns http success" do
-#       get :destroy
-#       expect(response).to have_http_status(:redirect)
-#     end
-#   end
+  describe "#create" do
+ 
+    it "creates a user" do
+      expect {
+        post :create, provider: :google
+      }.to change{ User.count }.by(1)
+    end
+ 
+    it "creates a session" do
+      expect(session[:user_id]).to be_nil
+      post :create, provider: :google
+      expect(session[:user_id]).to be_truthy
+    end
+ 
+    it "redirects to the root" do
+      post :create, provider: :google
+      expect(response).to redirect_to root_url
+    end
+ 
+  end
 
-# end
+  describe "GET #destroy" do
+    it "destroys a session" do
+      post :create, provider: :google
+      expect(session[:user_id]).to be_truthy
+      post :destroy
+      expect(session[:user_id]).to be_nil
+    end
+
+    it "redirects to the root" do
+      post :destroy
+      expect(response).to redirect_to root_url
+    end
+  end
+
+end
