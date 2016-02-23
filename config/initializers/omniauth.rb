ac42f354baa62d84c6018c4aa4a6f430a8b538a1
Brent Sullivan <brntsllvn@gmail.com>
2016-02-22 19:34:07 -0800

    Google varification

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,4 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, 
   !Rails.env.production? ? ENV["GOOGLE_CLIENT_ID_DEV"] : ENV["GOOGLE_CLIENT_ID_PROD"],
   !Rails.env.production? ? ENV["GOOGLE_CLIENT_SECRET_DEV"] : ENV["GOOGLE_CLIENT_SECRET_PROD"]
+
+  # config/initialisers/omniauth.rb
+  OmniAuth.config.full_host = Rails.env.production? ? 'https://atyd.herokuapp.com' : 'http://localhost:3000'
 end
