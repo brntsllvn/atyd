122a15dd04c34bbece9660baa333e1f2023a29c8
Brent Sullivan <brntsllvn@gmail.com>
2016-02-23 08:10:43 -0800

    Gibbon specs on hold until properly mocked

--- spec/models/mailchimp_gibbon_api_spec.rb
+++ spec/models/mailchimp_gibbon_api_spec.rb
@@ -1,46 +1,46 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe Gibbon::APIRequest, type: :model do
+# RSpec.describe Gibbon::APIRequest, type: :model do
 
-  describe "gibbon" do
-    before :each do
-      @gibbon = Gibbon::Request.new
-    end
+#   describe "gibbon" do
+#     before :each do
+#       @gibbon = Gibbon::Request.new
+#     end
 
-    it "returns instance of Gibbon::Request" do
-      expect(@gibbon).to be_an_instance_of(Gibbon::Request)
-    end
+#     it "returns instance of Gibbon::Request" do
+#       expect(@gibbon).to be_an_instance_of(Gibbon::Request)
+#     end
 
-    it "returns array of lists" do
-      lists = @gibbon.lists.retrieve
-      expect(lists).to be_a(Hash)
-      expect(lists.count).to eq 3 # my lists + mailchimp admin list
-    end
+#     it "returns array of lists" do
+#       lists = @gibbon.lists.retrieve
+#       expect(lists).to be_a(Hash)
+#       expect(lists.count).to eq 3 # my lists + mailchimp admin list
+#     end
 
-    it "returns test list" do
-      list = @gibbon.lists(ENV["MAILCHIMP_LIST_ID_TEST"]).retrieve 
-      expect(list['name']).to eq "rails list"
-      num_members = list['stats']['member_count']
-    end
+#     it "returns test list" do
+#       list = @gibbon.lists(ENV["MAILCHIMP_LIST_ID_TEST"]).retrieve 
+#       expect(list['name']).to eq "rails list"
+#       num_members = list['stats']['member_count']
+#     end
     
-    it "adds a user to the test list" do 
-      subscriber_hash = Digest::MD5.hexdigest("abc123@gmail.com".downcase)
-      params = {:status=>"subscribed", :language=>"en", :email_address=>"abc123@gmail.com", :merge_fields=>{:FNAME=>"Jimmy", :LNAME=>"Bean"}}
+#     it "adds a user to the test list" do 
+#       subscriber_hash = Digest::MD5.hexdigest("abc123@gmail.com".downcase)
+#       params = {:status=>"subscribed", :language=>"en", :email_address=>"abc123@gmail.com", :merge_fields=>{:FNAME=>"Jimmy", :LNAME=>"Bean"}}
 
-      @gibbon.lists(ENV["MAILCHIMP_LIST_ID_TEST"]).members(subscriber_hash).upsert(body: params)
+#       @gibbon.lists(ENV["MAILCHIMP_LIST_ID_TEST"]).members(subscriber_hash).upsert(body: params)
 
-      list = @gibbon.lists(ENV["MAILCHIMP_LIST_ID_TEST"]).retrieve 
-      num_members = list['stats']['member_count']
-      expect(num_members).to eq 1      
+#       list = @gibbon.lists(ENV["MAILCHIMP_LIST_ID_TEST"]).retrieve 
+#       num_members = list['stats']['member_count']
+#       expect(num_members).to eq 1      
 
-      # @gibbon.lists(ENV["MAILCHIMP_LIST_ID_TEST"]).members("bbb@bar.com").update(
-      #   body: { status: "unsubscribed" })
+#       # @gibbon.lists(ENV["MAILCHIMP_LIST_ID_TEST"]).members("bbb@bar.com").update(
+#       #   body: { status: "unsubscribed" })
 
-      # list = @gibbon.lists(ENV["MAILCHIMP_LIST_ID_TEST"]).retrieve 
-      # num_members = list['stats']['member_count']
+#       # list = @gibbon.lists(ENV["MAILCHIMP_LIST_ID_TEST"]).retrieve 
+#       # num_members = list['stats']['member_count']
 
-      # expect(num_members).to eq 0
-    end
+#       # expect(num_members).to eq 0
+#     end
     
-  end
-end
+#   end
+# end
