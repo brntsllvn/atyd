0c333886b1821e98472215d7fd743bb47efa6fd0
Brent Sullivan <brntsllvn@gmail.com>
2016-02-22 20:47:47 -0800

    gibbon specs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,16 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = user.id
+
+    # subscriber_hash = Digest::MD5.hexdigest(user.email.downcase)
+    # params = {:status=>"subscribed", 
+    #           :language=>"en", 
+    #           :email_address=>user.email.downcase, 
+    #           :merge_fields=>{:FNAME=>user.name, :LNAME=>"TEST"}}
+    
+    # @gibbon = Gibbon::Request.new
+    # @gibbon.lists(ENV["MAILCHIMP_LIST_ID_REAL"]).members(subscriber_hash).upsert(body: params)
+
     redirect_to root_path
   end
 
