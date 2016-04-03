4fe90fb7da39a58436c17f25fcd4119aac748f81
Brent Sullivan <brntsllvn@gmail.com>
2016-04-02 20:28:26 -0700

    grasping at straws... google oauth is ignoring me...

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,5 +5,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   !Rails.env.production? ? ENV["GOOGLE_CLIENT_ID_DEV"] : ENV["GOOGLE_CLIENT_ID_PROD"],
   !Rails.env.production? ? ENV["GOOGLE_CLIENT_SECRET_DEV"] : ENV["GOOGLE_CLIENT_SECRET_PROD"]
 
-  OmniAuth.config.full_host = Rails.env.production? ? 'http://acetheyellowdog.com' : 'http://localhost:3000'
+  OmniAuth.config.full_host = Rails.env.production? ? 'http://www.acetheyellowdog.com' : 'http://localhost:3000'
 end
